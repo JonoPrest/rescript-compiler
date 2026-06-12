bf7a2cf453f1c8a7352202377c1f72ddfd7c17fd
Jono Prest <jjprest@gmail.com>
2026-06-12 16:17:27 +0000

    dce: trim js stmt optionals

--- compiler/core/js_stmt_make.mli
+++ compiler/core/js_stmt_make.mli
@@ -30,10 +30,9 @@ type t = J.statement
 (* val empty_stmt :
    t *)
 
-val throw_stmt : ?comment:string -> J.expression -> t
+val throw_stmt : J.expression -> t
 
 val if_ :
-  ?comment:string ->
   ?declaration:Lam_compat.let_kind * Ident.t ->
   (* when it's not None, we also need make a variable declaration in the
      begininnig, however, we can optmize such case
@@ -50,7 +49,6 @@ val block : J.block -> t
 *)
 
 val int_switch :
-  ?comment:string ->
   ?declaration:Lam_compat.let_kind * Ident.t ->
   ?default:J.block ->
   J.expression ->
@@ -73,25 +71,17 @@ val int_switch :
 *)
 
 val string_switch :
-  ?comment:string ->
   ?declaration:Lam_compat.let_kind * Ident.t ->
   ?default:J.block ->
   J.expression ->
   (Ast_untagged_variants.tag_type * J.case_clause) list ->
   t
 
-val declare_variable :
-  ?comment:string ->
-  ?ident_info:J.ident_info ->
-  kind:Lam_compat.let_kind ->
-  Ident.t ->
-  t
+val declare_variable : kind:Lam_compat.let_kind -> Ident.t -> t
 (** Just declaration without initialization *)
 
 (*** Declaration with initialization *)
 val define_variable :
-  ?comment:string ->
-  ?ident_info:J.ident_info ->
   kind:Lam_compat.let_kind ->
   Ident.t ->
   J.expression ->
@@ -104,7 +94,7 @@ val define_variable :
    Ident.t ->
    t *)
 
-val assign : ?comment:string -> J.ident -> J.expression -> t
+val assign : J.ident -> J.expression -> t
 
 (** Used in cases like 
     {[
@@ -130,10 +120,9 @@ val assign : ?comment:string -> J.ident -> J.expression -> t
    J.ident ->
    t *)
 
-val while_ : ?comment:string -> ?label:J.label -> J.expression -> J.block -> t
+val while_ : ?label:J.label -> J.expression -> J.block -> t
 
 val for_ :
-  ?comment:string ->
   ?label:J.label ->
   J.for_ident_expression option ->
   J.finish_ident_expression ->
@@ -142,22 +131,16 @@ val for_ :
   J.block ->
   t
 
-val for_of :
-  ?comment:string -> ?label:J.label -> J.expression -> J.ident -> J.block -> t
+val for_of : ?label:J.label -> J.expression -> J.ident -> J.block -> t
 
 val for_await_of :
-  ?comment:string -> ?label:J.label -> J.expression -> J.ident -> J.block -> t
+  ?label:J.label -> J.expression -> J.ident -> J.block -> t
 
-val try_ :
-  ?comment:string ->
-  ?with_:J.ident * J.block ->
-  ?finally:J.block ->
-  J.block ->
-  t
+val try_ : ?with_:J.ident * J.block -> J.block -> t
 
-val exp : ?comment:string -> J.expression -> t
+val exp : J.expression -> t
 
-val return_stmt : ?comment:string -> J.expression -> t
+val return_stmt : J.expression -> t
 
 (* val return_unit : t list *)
 (** for ocaml function which returns unit 
