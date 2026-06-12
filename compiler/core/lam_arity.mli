d40e758ba18c566fc272742d2aeaed62d3be5ed7
Jono Prest <jjprest@gmail.com>
2026-06-12 13:00:35 +0000

    dce: trim arity debug

--- compiler/core/lam_arity.mli
+++ compiler/core/lam_arity.mli
@@ -32,12 +32,6 @@ type t = private
   *)
   | Arity_na
 
-val equal : t -> t -> bool
-
-val print : Format.formatter -> t -> unit
-
-val print_arities_tbl : Format.formatter -> (Ident.t, t ref) Hashtbl.t -> unit
-
 val merge : int -> t -> t
 
 val non_function_arity_info : t
