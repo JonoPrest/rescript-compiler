1255408f9c65338f99cb2853dbca58af5e71af51
Jono Prest <jjprest@gmail.com>
2026-06-12 14:23:10 +0000

    dce: trim arg spec variants

--- compiler/ext/bsc_args.ml
+++ compiler/ext/bsc_args.ml
@@ -26,17 +26,15 @@ type anon_fun = rev_args:string list -> unit
 
 type string_action =
   | String_call of (string -> unit)
-  | String_set of string ref
   | String_optional_set of string option ref
   | String_list_add of string list ref
 
 type unit_action =
   | Unit_call of (unit -> unit)
-  | Unit_lazy of unit lazy_t
   | Unit_set of bool ref
   | Unit_clear of bool ref
 
-type spec = Unit_dummy | Unit of unit_action | String of string_action
+type spec = Unit of unit_action | String of string_action
 
 exception Bad = Arg.Bad
 
@@ -108,13 +106,11 @@ let parse_exn ~usage ~argv ?(start = 1) (speclist : t)
       match Ext_spec.assoc3 speclist s with
       | Some action -> (
         match action with
-        | Unit_dummy -> ()
         | Unit r -> (
           match r with
           | Unit_set r -> r := true
           | Unit_clear r -> r := false
-          | Unit_call f -> f ()
-          | Unit_lazy f -> Lazy.force f)
+          | Unit_call f -> f ())
         | String f -> (
           if !current >= finish then
             stop_raise ~usage ~error:(Missing s) speclist
@@ -123,7 +119,6 @@ let parse_exn ~usage ~argv ?(start = 1) (speclist : t)
             incr current;
             match f with
             | String_call f -> f arg
-            | String_set u -> u := arg
             | String_optional_set s -> s := Some arg
             | String_list_add s -> s := arg :: !s))
       | None -> stop_raise ~usage ~error:(Unknown s) speclist
